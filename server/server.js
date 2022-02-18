7ea6c273a9149d1fd1965b9ec33d11b8543611f2
necmigunduz <necmigunduz@gmail.com>
2022-02-18 21:18:32 +0300

    Add homepage message to server

--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
-import express from 'express'
-import routes from './src/routes/routes.js'
-import mongoose from 'mongoose'
-import bodyParser from 'body-parser'
+import express from "express";
+import routes from "./src/routes/routes.js";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
-const app = express()
+const app = express();
 
-const PORT = process.env.SERVER_PORT || 4000
-mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/product-app')
+const PORT = process.env.SERVER_PORT || 4000;
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/product-app");
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-routes(app)
+routes(app);
+
+app.get("/", (req, res) => {
+  res.send("This is the homepage for the Restful API for products");
+});
 
 app.listen(PORT, () => {
-    console.log('Server is running on port: ' + PORT)
-})
+  console.log("Server is running on port: " + PORT);
+});
