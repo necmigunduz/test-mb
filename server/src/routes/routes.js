6429841ba12ee4b66e818d1eed6db379c7fcae6a
necmigunduz <necmigunduz@gmail.com>
2022-02-18 21:15:33 +0300

    Add routes

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,1 +1,14 @@
+import { addNewProduct, getAllProducts, getProduct, updateProduct, deleteProduct } from '../controllers/controller.js'
 
+const routes = (app) => {
+    app.route('/products')
+        .get(getAllProducts)
+        .post(addNewProduct)
+    app.route('/products/:id')
+        .get(getProduct)
+        .put(updateProduct)
+        .patch(updateProduct)
+        .delete(deleteProduct)
+}
+
+export default routes
