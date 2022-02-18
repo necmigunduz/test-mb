401773413cc1f0d37df63114870744a4ceebd907
necmigunduz <necmigunduz@gmail.com>
2022-02-18 20:56:23 +0300

    Add routes and model

---
+++ server/src/models/model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema();
+
+const ProductSchema = new Schema({
+    name: {
+        type: String,
+        required: 'Product name is required!'
+    },
+    brand: {
+        type: String,
+        required: 'Brand is required!'
+    },
+    color: {
+        type: String
+    },
+    desc: {
+        type: String
+    },
+    price: {
+        type: Number
+    },
+    amount: {
+        type: Number
+    }
+})
+
+export default ProductSchema
